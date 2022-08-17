4c8a28fc0ab3a7c40ae9cef005299a7645df16ea
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-08-17 22:08:15 +0600

    add

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className="flex items-center justify-center h-screen font-bold">
+      <div className="flex flex-col items-center justify-center h-screen font-bold">
         <h1 className="text-orange-500 text-6xl">Welcome to Prime</h1>
+        <h1 className="text-orange-500 text-6xl">Tanvir Hasan Sohan</h1>
       </div>
     </div>
   );
