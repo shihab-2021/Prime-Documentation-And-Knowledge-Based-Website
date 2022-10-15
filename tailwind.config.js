d87aee319a2e471b20f3e4bcb3fb6714ee5c125e
Shajibul Alam Shihab <82690571+shihab-2021@users.noreply.github.com>
2022-10-15 22:38:44 +0600

    community card complite

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: "#6b707f",
+        darkBlue: "#2C303A",
+        primary: "#000",
+        buttons: "#0000ff",
+        lightBlue: "#4C4CF1",
+        lightDark: "#131417",
+        offWhite: "#F7F5F5",
+        DarkGray: "#1d1e25",
+        Dark: "#2c303a",
+      },
+    },
   },
   plugins: [],
 };
