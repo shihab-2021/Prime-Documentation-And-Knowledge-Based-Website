e844cf530d12b3ac79a94f4957bb706a227877c8
Shajibul Alam Shihab <82690571+shihab-2021@users.noreply.github.com>
2022-10-19 14:24:52 +0600

    about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import MainAbout from "../components/About/MainAbout/MainAbout";
+
+const about = () => {
+    return (
+        <div>
+            <MainAbout></MainAbout>
+        </div>
+    );
+};
+
+export default about;
