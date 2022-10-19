15f46772f15209a43a7544b197c91b2fb78916d4
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-10-19 22:18:25 +0600

    About add

--- components/About/MainAbout/MainAbout.js
+++ components/About/MainAbout/MainAbout.js
@@ -1,5 +1,84 @@
+/* eslint-disable @next/next/no-img-element */
+
+
 const MainAbout = () => {
-  return <div>This is about page</div>;
+
+
+
+  return <div >
+                 <div className=" bg-sky-500">
+                     <div className="pt-48 pb-32 text-center container mx-auto">
+                         <h1 className="text-5xl">About Us</h1>
+                     </div>
+                </div>
+
+                <div className="container mx-auto sm:grid grid-cols-2 mt-10   ">
+                     <div className="w-full">
+                         <img className="w-11/12" src="about.png" alt="" />
+                     </div>
+                     <div className="w-96">
+                         <h2 className="text-3xl">
+                              A united world, connected by technology
+                         </h2>
+                         <ul className="list-disc">
+                             <li className="my-2">
+                                 It is a long established fact that a reader will be distracted by the readable content of a page when looking.
+                             </li>
+                             <li className="my-2">
+                                 The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
+                             </li>
+                             <li className="my-2">
+
+                                Many desktop publishing packages and web page editors now use Lorem Ipsum.
+                             </li>
+                         </ul>
+                        <hr />
+                        <p className="mb-0">
+                              “Many desktop publishing packages and web page editors now
+                               use Lorem Ipsum as their default model text, and a search
+                              for will uncover many web sites still in their
+                              infancy.”
+                       </p>
+                     </div>
+                
+                </div>
+
+                <div className="container mx-auto text-center my-10 w-96">
+                  <h2 className="text-3xl">Meet our (awesome) team</h2>
+                  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                </div>
+
+                <div className="grid grid-cols-3 items-center text-center container mx-auto">
+                  <div><hr /></div>
+                  <div className="text-2xl">Team Member</div>
+                  <div> <hr /></div>
+
+                </div>
+
+                <div className="sm:flex justify-center container mx-auto my-10">
+                        <div className="mx-5 card ">
+                           <img style={ { width: '200px', height:'200px' }} src="sohan.jpeg" alt="" />
+                           <h3 className="font-bold">Tanvir Hasan Sohan</h3>
+                           <p>Junior web Developer</p>
+                        </div>
+                        <div className="mx-5" style={{ width: '200px', height:'200px' }}>
+                           <img style={{ width: '200px', height:'200px' }}  src="shihab.jpeg" alt="" />
+                           <h3 className="font-bold">Shajibul Alam Shihab</h3>
+                           <p>Junior web Developer</p>
+                        </div>
+                        <div className="mx-5" style={{ width: '200px', height:'200px' }}>
+                           <img style={{ width: '200px', height:'200px' }} src="mishon.jpeg" alt="" />
+                           <h3 className="font-bold">Muhtakim Safat Mishon</h3>
+                           <p>Junior web Developer</p>
+                        </div>
+  
+                          
+                </div>
+   
+    
+
+
+         </div>;
 };
 
 export default MainAbout;
