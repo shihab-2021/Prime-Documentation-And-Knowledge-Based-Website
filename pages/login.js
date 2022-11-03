4c3a82a5b9c452f82f0c6b7b7f629345eba4f9f6
Shajibul Alam Shihab <82690571+shihab-2021@users.noreply.github.com>
2022-11-03 15:59:46 +0600

    login register connected

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,10 @@
 import React from "react";
+import LoginMain from "../components/Login/LoginMain";
 
 const login = () => {
-  return <div>This yes this is login page should be updated</div>;
+  return <div>
+    <LoginMain></LoginMain>
+  </div>;
 };
 
 export default login;
